8a83158ef077299d946530a0755d05e539b8b590
Freakqency <suryashankar649@gmail.com>
2022-04-25 14:05:17 -0400

    Functions  To Place Current Player Mark

--- main.js
+++ main.js
@@ -2,3 +2,25 @@
 // 2. Loose remove the beads
 // 3. Win add 3 more beads
 // 4. Draw 1 beads
+
+const cellElements = document.querySelectorAll('[data-cell]')
+const board = document.getElementById('board')
+const X_CLASS = 'x'
+const CIRCLE_CLASS = 'circle'
+let circleTurn
+
+cellElements.forEach(cell => {
+	cell.addEventListener('click', handleClick, { once: true })
+})
+
+function handleClick(e) {
+	const cell = e.target
+	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+	placeMark(cell, currentClass)
+	//check For Win
+	//Check for marks
+}
+
+function placeMark(cell, currentClass) {
+	cell.classList.add(currentClass)
+}
