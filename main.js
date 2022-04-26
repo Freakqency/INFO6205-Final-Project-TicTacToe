844a86b9af2cf890f12e6df836058e632a132fe6
Thomas John <john.th@northeastern.edu>
2022-04-26 18:44:14 -0400

    hash key generator and matchbox init

--- main.js
+++ main.js
@@ -20,12 +20,36 @@ const WINNING_COMBINATIONS = [
 	[2, 4, 6],
 ]
 
+var matchBoxes = [];
+
 let stats = {"menaceWin":0, "menaceDraw":0, "menaceLost":0, "totalMatch":0, "gameHistory":[]}
 gameInfo = {"startTime": Date.UTC}
 
 let circleTurn
 
-startGame()
+
+let gameCells = 9
+
+let getAllGameStates = gameCells => {
+    let possibleMoves = '012'.split('') // X --> 1, O --> 2, empty position --> 0
+    let lengthen = word => possibleMoves.map(letter => word + letter)
+    let addLetters = words => flatten(words.map(lengthen))
+    let _getAllWords = (letters, words = possibleMoves, current = 1) => {
+        return letters == current ? words : 
+            _getAllWords(letters, addLetters(words), current + 1)
+    }
+
+    return _getAllWords(gameCells)
+}
+
+function fillMatchBox(){
+	gameStates = getAllGameStates(gameCells)
+	for (const element of gameStates) {
+		move = {element:[1,2,3,4,5,6,7,8,9]}
+		matchBoxes.push(move)
+	}
+	console.log("matchboxes ", matchBoxes)
+}
 
 cellElements.forEach(cell => {
 	cell.addEventListener('click', handleClick, { once: true })
@@ -37,6 +61,8 @@ function startGame() {
 		cell.addEventListener('click', handleClick, { once: true })
 	})
 	setBoardHoverClass
+	fillMatchBox();
+	console.log("matchbox  ", matchBoxes);
 }
 
 function handleClick(e) {
@@ -97,18 +123,7 @@ function checkWin(currentClass) {
 
 let flatten = arr => arr.reduce((carry, item) => carry.concat(item), [])
 
-let gameCells = 9
 
-let getAllGameStates = gameCells => {
-    let possibleMoves = '012'.split('') // X --> 1, O --> 2, empty position --> 0
-    let lengthen = word => possibleMoves.map(letter => word + letter)
-    let addLetters = words => flatten(words.map(lengthen))
-    let _getAllWords = (letters, words = possibleMoves, current = 1) => {
-        return letters == current ? words : 
-            _getAllWords(letters, addLetters(words), current + 1)
-    }
-
-    return _getAllWords(gameCells)
-}
+startGame()
 
 
