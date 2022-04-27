e0920f10f0f2bdd82bc8ddbd0abe32280e975a83
Thomas John <john.th@northeastern.edu>
2022-04-27 01:46:24 -0400

    menace2 basics

--- main.js
+++ main.js
@@ -25,7 +25,13 @@ const GAMMA = -1
 const DELTA = 1
 var record_moves = new Array()
 var record_pos = new Array()
+
+var record_movesM2 = new Array()
+var record_posM2 = new Array()
+
+
 var mv
+var mvM2
 
 var matchBoxes = [];
 var matchBoxesMenace2 = [];
@@ -60,6 +66,9 @@ function fillMatchBox() {
 			if (element[i] == '0') temp.push(i)
 		}
 		matchBoxes[element] = temp
+		if(gameType=2){
+			matchBoxesMenace2[element] = temp
+		}
 	}
 	logGame("Making the matchboxes and beads ready for you...")
 }
@@ -76,6 +85,18 @@ function getBead(currentBoardState) {
 	mv++
 	return currentMove
 }
+
+function getBeadMenace2(currentBoardState) {
+	var beads = matchBoxesMenace2[currentBoardState]
+	var currentMove = random_item(beads)
+	record_pos[mvM2] = currentBoardState
+	record_moves[mvM2] = currentMove
+	mvM2++
+	return currentMove
+}
+
+
+
 
 cellElements.forEach(cell => {
 	cell.addEventListener('click', handleClick, { once: true })
@@ -107,6 +128,22 @@ function playMenace() {
 		setBoardHoverClass()
 	}
 }
+
+function playMenace2() {
+	const currentClass = circleTurn ? CIRCLE_CLASS : X_CLASS
+	var currentBoardState = getBoardState()
+	var currentMove = getBeadMenace2(currentBoardState)
+	console.log(currentMove)
+	placeMarkMenace(currentMove, currentClass)
+	if (checkWin(currentClass)) endGame(false);
+	else if (isDraw()) endGame(true)
+	else {
+		swapTurns()
+		setBoardHoverClass()
+	}
+}
+
+
 
 function handleClick(e) {
 	const cell = e.target
@@ -229,10 +266,18 @@ function logGame(message) {
 let flatten = arr => arr.reduce((carry, item) => carry.concat(item), [])
 
 function postmortem() {
-	if (winner == "X") var adjacements = BETA
-	else if (winner == "O") var adjacements = GAMMA
-	else if (winner == "Draw") var adjacements = DELTA
+	var adjacements
+	if (winner == "X") adjacements = BETA
+	else if (winner == "O") adjacements = GAMMA
+	else if (winner == "Draw") adjacements = DELTA
 	for (let i = 0; i < mv; i++) matchBoxes[record_pos[i]][record_moves[i]] += adjacements
+	if(gameType==2){
+		if (winner == "O") adjacements = BETA
+		else if (winner == "X") adjacements = GAMMA
+		else if (winner == "Draw") adjacements = DELTA
+		for (let j = 0; j < mvM2; j++) matchBoxesMenace2[record_posM2[i]][record_movesM2[i]] += adjacements
+	}
+	
 }
 
 function publishgraphs(){
